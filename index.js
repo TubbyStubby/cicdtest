8ddfb2b302918a30c16a8fe0ce54ba408cf7054e
TubbyStubby <tubbystubby2@gmail.com>
2021-11-12 00:44:18 +0530

    loads of stuff

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const router = express.Router();
+require('redis-helper');
+
 app.use('/', require('./routes'));
 app.listen(port);
--- routes.js
+++ routes.js
@@ -1,3 +1,16 @@
 const router = require('express').Router();
-router.get('/', (req, res, next) => { res.send('Hello World!') });
+const rclient = require('./redis-helper');
+
+router.get('/', async (req, res, next) => { res.send('Hello World!') });
+router.post('/cache/:stuff', async (req, res) => {
+    let result = await rclient.setAsync('testkey', req.params.stuff);
+    console.log(result);
+    return res.send(result);
+});
+router.get('/cache/:stuff', async (req, res) => {
+    let result = await rclient.getAsync('testkey');
+    console.log(result);
+    return res.send(result);
+});
+
 module.exports = router;
