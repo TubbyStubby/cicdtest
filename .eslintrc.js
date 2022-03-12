13edc00efaf6650030271ec1c70226fdf89308c5
TubbyStubby <tubbystubby2@gmail.com>
2022-03-13 00:55:11 +0530

    liniting

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "commonjs": true,
+        "node": true,
+        "es2015": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+    }
+}
